34eb2db8832a546d91ed773501f82251db05abc9
Ruslan Abliazov <r.abliazov@gmail.com>
2023-02-18 19:17:06 +0300

    just code for excersise

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-import WithoutArrays
+# import WithoutArrays
+# these code is just training for python course
+def get_sum_of_numerals(text: str) -> int:
+    def is_numeral(char: str) -> bool:
+        if len(char) == 1 and char in '0123456789':
+            return True
+    i = 0
+    ans = 0
+
+    while i < len(text):
+        if is_numeral(text[i]):
+            ans += int(text[i])
+            i += 1
+        while i < len(text) and not is_numeral(text[i]):
+            i += 1
+    return ans
+
+
+print(get_sum_of_numerals('ah-ha123abrcadabra'))
